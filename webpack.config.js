a88207637359bec0abbc0993b5e55f417d6d64d1
Martin Bonchev <martin.bonchevv@gmail.com>
2023-03-08 22:11:31 +0200

    feat: implement webpack config

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+
+module.exports = {
+  entry: ["./src/ts/index.ts", "./src/css/styles.css"],
+  devtool: "inline-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.ts?$/i,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ["", ".ts", ".js", ".css"],
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "our project",
+      template: "src/index.html",
+      favicon: "src/assets/favicon.ico",
+    }),
+  ],
+  optimization: {
+    minimizer: [new CssMinimizerPlugin(), new TerserPlugin()],
+  },
+  devServer: {
+    static: [
+      path.join(__dirname, "dist"),
+      "./src/index.html",
+      "./src/css/style.css",
+    ],
+    compress: true,
+    liveReload: true,
+    hot: true,
+    port: 3000,
+  },
+};
