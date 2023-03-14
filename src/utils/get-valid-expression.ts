997944f07f49523af015b35bf3606bf52dc831f6
Martin Bonchev <martin.bonchevv@gmail.com>
2023-03-14 23:56:58 +0200

    fix: regex for validation

--- src/utils/get-valid-expression.ts
+++ src/utils/get-valid-expression.ts
@@ -1,18 +1,19 @@
-const TEST_EXPRESSION_PATTERN = /^[\d+\-\+\*\/()]*$/g;
+const TEST_EXPRESSION_PATTERN =
+  /^(\d|(\(+\d+)*)+([+\-*/]?(\d+|(\(+\d+))|\))*$/g;
 
 export default function getValidExpression(expression: string) {
   if (expression.length === 0) {
     throw new Error("Please provide an input!");
   }
 
-  if (!TEST_EXPRESSION_PATTERN.test(expression)) {
-    throw new Error("Invalid input!");
-  }
-
   if (expression.split(")").length !== expression.split("(").length) {
     throw new Error(
       "The provided input contains a different number of parentheses!"
     );
+  }
+
+  if (!TEST_EXPRESSION_PATTERN.test(expression)) {
+    throw new Error("Invalid input!");
   }
 
   return expression;
